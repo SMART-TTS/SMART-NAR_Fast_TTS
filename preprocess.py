10c1d43c1b3e3aa3ca53463f323e91b37ab140c2
leemoa12 <leemoa12@gmail.com>
2020-11-07 13:38:45 +0900

    revised preprocess

--- preprocess.py
+++ preprocess.py
@@ -3,30 +3,21 @@ import numpy as np
 from tqdm import tqdm
 
 from utils.data_utils import _DataLoader, guide_attention
-from utils.plot import plot_spectrum, plot_attention
-from utils import TimeVaryingMetaTemplate
 
 
 def preprocess(conf):
     print("pre-processing...")
-    # validation_files training_files
-    TVMT = TimeVaryingMetaTemplate(40)
     loader = _DataLoader(conf['data']['training_files'], conf)
     is_norm = conf['data']['is_norm']
     with open(conf['data']['training_files'], 'r') as f:
         lines = f.readlines()
         for line in tqdm(lines):
-            if 'chj' in conf['data']['data_name']:
-                fname, txt = line.split('|')
-            elif conf['data']['data_name'] == 'lj':
-                fname, txt = line.split('|')
-                fname = os.path.join('db', 'LJSpeech-1.1', 'wavs', '{}.wav'.format(fname))
+            fname, txt = line.split('|')
+            fname = os.path.join('db', 'SMART_TTS_20201105', 'FPHJ0', 'wav_22050', '{}.wav'.format(fname))
 
             mel = loader.get_mel(fname)
             text = loader.get_text(txt.rstrip())
 
-            if 'chj' in conf['data']['data_name']:
-                folder = fname.split(os.path.sep)[-2]
             name, ext = os.path.splitext(os.path.basename(fname))
 
             if not os.path.exists(os.path.join(conf['data']['featdir'], conf['data']['data_name'])):
@@ -39,10 +30,6 @@ def preprocess(conf):
             if not os.path.exists(mel_path):
                 os.makedirs(mel_path)
 
-            if 'chj' in conf['data']['data_name']:
-                mel_path = os.path.join(mel_path, folder)
-                if not os.path.exists(mel_path):
-                    os.makedirs(mel_path)
             # plot_spectrum(mel, 'mel')
             np.save(os.path.join(mel_path, name + ".npy"), mel.astype(np.float32))
 
@@ -58,27 +45,8 @@ def preprocess(conf):
             if not os.path.exists(coarse_mel_path):
                 os.makedirs(coarse_mel_path)
 
-            if 'chj' in conf['data']['data_name']:
-                coarse_mel_path = os.path.join(coarse_mel_path, folder)
-                if not os.path.exists(coarse_mel_path):
-                    os.makedirs(coarse_mel_path)
-
-            # print(os.path.join(coarse_mel_path, name + ".npy"))
             # plot_spectrum(coarse_mel, 'coarse_mel')
             np.save(os.path.join(coarse_mel_path, name + ".npy"), coarse_mel.astype(np.float32))
-
-            coarse_t = coarse_mel.shape[1]
-            tvmt = TVMT(coarse_t, len(text))
-            tvmt_path = os.path.join(conf['data']['featdir'], conf['data']['data_name'], 'tvmt')
-            if not os.path.exists(tvmt_path):
-                os.makedirs(tvmt_path)
-
-            if 'chj' in conf['data']['data_name']:
-                tvmt_path = os.path.join(tvmt_path, folder)
-                if not os.path.exists(tvmt_path):
-                    os.makedirs(tvmt_path)
-            tvmt = tvmt.detach().cpu().numpy()
-            np.save(os.path.join(tvmt_path, name + ".npy"), tvmt.astype(np.float32))
 
             guide, mask = guide_attention([len(text)], [coarse_mel.shape[-1]],
                                           conf['train']['guided_attention']['guide_g'],
@@ -88,26 +56,24 @@ def preprocess(conf):
             guide_path = os.path.join(conf['data']['featdir'], conf['data']['data_name'], 'guide')
             if not os.path.exists(guide_path):
                 os.makedirs(guide_path)
-
-            if 'chj' in conf['data']['data_name']:
-                guide_path = os.path.join(guide_path, folder)
-                if not os.path.exists(guide_path):
-                    os.makedirs(guide_path)
-
             # plot_attention(guide[0], 'guide')
             np.save(os.path.join(guide_path, name + ".npy"), guide[0].astype(np.float32))
 
             mask_path = os.path.join(conf['data']['featdir'], conf['data']['data_name'], 'mask')
             if not os.path.exists(mask_path):
                 os.makedirs(mask_path)
-
-            if 'chj' in conf['data']['data_name']:
-                mask_path = os.path.join(mask_path, folder)
-                if not os.path.exists(mask_path):
-                    os.makedirs(mask_path)
-
             # plot_attention(mask[0], 'mask')
             np.save(os.path.join(mask_path, name + ".npy"), mask[0].astype(np.float32))
+
+            mask2 = attn_mask([len(text)], [coarse_mel.shape[-1]], 0.15,
+                              conf['data']['data_max_text_length'],
+                              conf['data']['data_max_mel_length'])
+            mask2_path = os.path.join(conf['data']['featdir'], conf['data']['data_name'], 'mask_for_attn_masking')
+            if not os.path.exists(mask2_path):
+                os.makedirs(mask2_path)
+            np.save(os.path.join(mask2_path, name + ".npy"), mask2[0].astype(np.float32))
+            np.save(mask2_path, mask2[0])
+
 
 def attn_mask(text_lengths, mel_lengths, guide_g=0.2, r=None, c=None):
     b = len(text_lengths)
@@ -129,97 +95,3 @@ def attn_mask(text_lengths, mel_lengths, guide_g=0.2, r=None, c=None):
                     w = np.exp(-((float(n - N - 1) / N) ** 2 / (2.0 * (guide_g ** 2))))
                     W[n][t] = w
     return mask
-
-if __name__ == "__main__":
-    import torch
-    from utils.text import text_to_sequence
-
-
-    # mel = np.load('/media/tts/f31fb3cf-8485-4868-8b63-75c78781b13d/App/kss_v_1.4-mel-4_5628.npy')
-    # plot_spectrum(mel, '123123123')
-
-    # make attn_mask for attn masking
-    # with open('/media/tts/f31fb3cf-8485-4868-8b63-75c78781b13d/App/asmltts_beta/db/chj_22050/validation.txt', 'r') as f:
-    #     lines = f.readlines()
-    #     for line in tqdm(lines):
-    #         fname, txt = line.split('|')
-    #         mel_path = fname.replace('db/chj_22050', 'feats/chj_22050/coarse_mels_norm')
-    #         mel_path = mel_path.replace('wav', 'npy')
-    #         mel = np.load(mel_path)
-    #         T = mel.shape[1]
-    #         text = torch.IntTensor(text_to_sequence(txt.rstrip()))
-    #         N = len(text)
-    #
-    #         mask = attn_mask([N], [T], 0.15, 400, 480)
-    #
-    #         mask_path = mel_path.replace('coarse_mels_norm', 'mask_for_attn_masking')
-    #         np.save(mask_path, mask[0])
-
-
-
-
-    # with open('/media/tts/f31fb3cf-8485-4868-8b63-75c78781b13d/App/asmltts_beta/db/LJSpeech-1.1/train.txt', 'w') as f2:
-    #     with open('/media/tts/f31fb3cf-8485-4868-8b63-75c78781b13d/App/asmltts_beta/db/LJSpeech-1.1/metadata.csv',
-    #               'r') as f:
-    #         lines = f.readlines()
-    #         for line in tqdm(lines):
-    #             fname, _, txt = line.split('|')
-    #             f2.write(fname + '|' + txt)
-
-    # get rs
-    # with open('db/chj_22050/train.txt', 'r') as f:
-    #     lines = f.readlines()
-    #     rs_min = 50
-    #     rs_max = 0
-    #     rs_list = []
-    #     for line in tqdm(lines):
-    #         fname, txt = line.split('|')
-    #
-    #         audiopath_and_text = [fname, txt.rstrip()]
-    #         text = torch.IntTensor(text_to_sequence(txt.rstrip()))
-    #         mel_path = fname.replace('db/chj_22050', 'feats/chj_22050/coarse_mels_norm')
-    #         mel_path = mel_path.replace('wav', 'npy')
-    #         mel = np.load(mel_path)
-    #
-    #         len_mel = mel.shape[1]
-    #         rs = float(len_mel / len(text))
-    #         rs_min = rs if rs < rs_min else rs_min
-    #         rs_max = rs if rs > rs_max else rs_max
-    #         # rs_list.append(rs)
-    #
-    #     print(rs_min, rs_max)
-
-    # Trimming
-    # import librosa
-    # with open('db/chj_22050/validation.txt', 'r') as f:
-    #     lines = f.readlines()
-    #     for line in tqdm(lines):
-    #         fname, txt = line.split('|')
-    #         # fname_in = fname
-    #         fname_in = fname.replace('_trim.wav', '.wav')
-    #         audio, rate = librosa.load(fname_in, sr=22050)
-    #         audio, _ = librosa.effects.trim(audio, top_db=20)
-    #         # audio = (audio * 32767).astype(int)
-    #
-    #         out_name = fname
-    #         # out_name = fname.replace('.wav', '_trim.wav')
-    #         librosa.output.write_wav(out_name, audio, rate)
-    #         # scipy.io.wavfile.write(out_name, 22050, audio)
-
-    # resample
-    # import librosa
-    # with open('db/chj/train.txt', 'r') as f:
-    #     lines = f.readlines()
-    #     for line in tqdm(lines):
-    #         fname, txt = line.split('|')
-    #         # fname_in = fname
-    #         fname_in = fname.replace('_trim.wav', '.wav')
-    #         audio, rate = librosa.load(fname_in, sr=48000)
-    #         audio_ = librosa.resample(audio, rate, 22050)
-    #         # audio = (audio * 32767).astype(int)
-    #
-    #         out_name = fname.replace('chj', 'chj_22050')
-    #
-    #         # out_name = fname.replace('.wav', '_trim.wav')
-    #         librosa.output.write_wav(out_name, audio_, 22050)
-    #         # scipy.io.wavfile.write(out_name, 22050, audio)
