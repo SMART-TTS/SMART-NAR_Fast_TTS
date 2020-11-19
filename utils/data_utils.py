aefc9f128464a4ba072b7d720b6487cc580ca758
leemoa12 <leemoa12@gmail.com>
2020-11-19 15:50:04 +0900

    data_utils

--- utils/data_utils.py
+++ utils/data_utils.py
@@ -34,10 +34,12 @@ class _DataLoader(torch.utils.data.Dataset):
         self.reduction_factor = data_conf['reduction_factor'] if data_conf['reduction_factor'] else None
         self.segment_length = data_conf['segment_length'] if data_conf['segment_length'] else None
         self.text_cleaners = data_conf['text_cleaners'] if data_conf['text_cleaners'] else None
+        self.mel_segment_length = self.segment_length // self.hop_length if data_conf['segment_length'] else None
 
         self.use_audio = True if 'audio' in data_conf['batch'] else False
         self.use_audio_seg = True if 'audio_seg' in data_conf['batch'] else False
         self.use_mel_seg = True if 'mel_seg' in data_conf['batch'] else False
+        self.use_mel_seg_tag = True if 'mel_seg_tag' in data_conf['batch'] else False
         self.use_coarse_mel = True if self.reduction_factor is not None and self.reduction_factor > 1 else False
         self.use_mel = True if 'mel' in data_conf['batch'] else False
         self.use_text = True if 'text' in data_conf['batch'] else False
@@ -49,8 +51,9 @@ class _DataLoader(torch.utils.data.Dataset):
         self.load_mel_from_disk = conf['load_mel_from_disk']
         self.audiopaths = load_filepaths_and_text(audiopaths)
 
-        random.seed(1234)
-        random.shuffle(self.audiopaths)
+        if not valid:
+            random.seed(1234)
+            random.shuffle(self.audiopaths)
 
     def get_segment_wav(self, audiopath):
         audio, rate = self.get_wav(audiopath)
@@ -59,12 +62,15 @@ class _DataLoader(torch.utils.data.Dataset):
             max_audio_start = audio.shape[0] - self.segment_length
             audio_start = random.randint(0, max_audio_start)
             audio_seg = audio[audio_start : audio_start + self.segment_length]
+            mel_start = int(audio_start / self.hop_length)
+            mel_end = mel_start + self.mel_segment_length
         else:
             audio_seg = F.pad(
                 audio, (0, self.segment_length - audio.shape[0]), "constant"
             ).data
+            mel_start, mel_end = None, None
 
-        return audio_seg, rate
+        return audio_seg, rate, mel_start, mel_end
 
     def get_wav(self, audiopath):
         audio, rate = librosa.core.load(audiopath, sr=self.sampling_rate)
@@ -84,6 +90,10 @@ class _DataLoader(torch.utils.data.Dataset):
                         mel_path = mel_path.replace('wav', 'npy')
                     elif self.data_name == 'lj':
                         mel_path = os.path.join('feats', 'lj', 'mels_norm', '{}.npy'.format(audiopath))
+                    elif self.data_name == 'SMART_TTS':
+                        mel_path = audiopath.replace('db/SMART_TTS_20201105', 'feats/{}/mels_norm'.format(self.data_name))
+                        mel_path = mel_path.replace('FPHJ0/wav_22050/', '')
+                        mel_path = mel_path.replace('.wav', '.npy')
                     mel = np.load(mel_path)
 
                 else:
@@ -92,6 +102,10 @@ class _DataLoader(torch.utils.data.Dataset):
                         mel_path = mel_path.replace('wav', 'npy')
                     elif self.data_name == 'lj':
                         mel_path = os.path.join('feats', 'lj', 'coarse_mels_norm', '{}.npy'.format(audiopath))
+                    elif self.data_name == 'SMART_TTS':
+                        mel_path = audiopath.replace('db/SMART_TTS_20201105', 'feats/{}/coarse_mels_norm'.format(self.data_name))
+                        mel_path = mel_path.replace('FPHJ0/wav_22050/', '')
+                        mel_path = mel_path.replace('.wav', '.npy')
                     mel = np.load(mel_path)
 
         else:
@@ -133,6 +147,16 @@ class _DataLoader(torch.utils.data.Dataset):
         elif self.data_name == 'lj':
             guide_path = os.path.join('feats', 'lj', 'guide', '{}.npy'.format(audiopath))
             mask_path = os.path.join('feats', 'lj', 'mask', '{}.npy'.format(audiopath))
+        elif self.data_name == 'SMART_TTS':
+            guide_path = audiopath.replace('db/SMART_TTS_20201105', 'feats/{}/guide'.format(self.data_name))
+            guide_path = guide_path.replace('FPHJ0/wav_22050/', '')
+            guide_path = guide_path.replace('.wav', '.npy')
+            mask_path = audiopath.replace('db/SMART_TTS_20201105', 'feats/{}/mask'.format(self.data_name))
+            mask_path = mask_path.replace('FPHJ0/wav_22050/', '')
+            mask_path = mask_path.replace('.wav', '.npy')
+            mask2_path = audiopath.replace('db/SMART_TTS_20201105', 'feats/{}/mask_for_attn_masking'.format(self.data_name))
+            mask2_path = mask2_path.replace('FPHJ0/wav_22050/', '')
+            mask2_path = mask2_path.replace('.wav', '.npy')
         attn_guide = np.load(guide_path)
         attn_mask = np.load(mask_path)
         attn_mask2 = np.load(mask2_path)
@@ -149,13 +173,25 @@ class _DataLoader(torch.utils.data.Dataset):
         return torch.from_numpy(tvmt)
 
     def get_batch(self, audiopath_and_text):
-        audiopath = audiopath_and_text[0]
-        text = audiopath_and_text[1]
+        if len(audiopath_and_text) > 1:
+            audiopath = audiopath_and_text[0]
+            text = audiopath_and_text[1]
+
+            if self.data_name == 'SMART_TTS':
+                audiopath = os.path.join('db', 'SMART_TTS_20201105', 'FPHJ0', 'wav_22050', audiopath + '.wav')
+        else:
+            audiopath = None
+            if audiopath is not None:
+                if self.data_name == 'SMART_TTS':
+                    audiopath = os.path.join('db', 'SMART_TTS_20201105', 'FPHJ0', 'wav_22050', audiopath + '.wav')
+            text = audiopath_and_text[0]
+
         if self.use_audio_seg:
             if self.is_valid:
                 audio_seg, sr = self.get_wav(audiopath)
+                mel_start, mel_end = None, None
             else:
-                audio_seg, sr = self.get_segment_wav(audiopath)
+                audio_seg, sr, mel_start, mel_end = self.get_segment_wav(audiopath)
         else:
             audio_seg = None
         if self.use_audio:
@@ -170,16 +206,17 @@ class _DataLoader(torch.utils.data.Dataset):
         if self.use_attn_guide:
             attn_guide, attn_mask, attn_mask2 = self.get_attn_guides_and_masks(audiopath)
         else:
-            attn_guide, attn_mask = None, None
+            attn_guide, attn_mask, attn_mask2 = None, None, None
         if audio_seg is not None:
             audio_seg = torch.from_numpy(audio_seg).float()
         tvmt = self.get_time_varying_meta_template(audiopath) if self.use_tvmt else None
+        mel_seg_tag = [mel_start, mel_end] if self.use_mel_seg_tag else None
 
         return audio, audio_seg,\
                mel, mel_seg, coarse_mel, \
                text, \
                attn_guide, attn_mask, \
-               tvmt, attn_mask2
+               tvmt, attn_mask2, mel_seg_tag
 
     def __getitem__(self, index):
         try:
@@ -194,7 +231,8 @@ class _DataLoader(torch.utils.data.Dataset):
 class _DataCollate():
     """ Zero-pads model inputs and targets based on number of frames per setep
     """
-    def __init__(self, conf):
+    def __init__(self, conf, valid=False):
+        self.is_valid = valid
         data_conf = conf['data']
         self.reduction_factor = data_conf['reduction_factor'] if data_conf['reduction_factor'] else None
 
@@ -207,6 +245,7 @@ class _DataCollate():
         self.use_audio = True if 'audio' in data_conf['batch'] else False
         self.use_audio_seg = True if 'audio_seg' in data_conf['batch'] else False
         self.use_mel_seg = True if 'mel_seg' in data_conf['batch'] else False
+        self.use_mel_seg_tag = True if 'mel_seg_tag' in data_conf['batch'] else False
         self.use_coarse_mel = True if self.reduction_factor is not None and self.reduction_factor > 1 else False
         self.use_mel = True if 'mel' in data_conf['batch'] else False
         self.use_text = True if 'text' in data_conf['batch'] else False
@@ -235,11 +274,20 @@ class _DataCollate():
                 text = batch[ids_sorted_decreasing[i]][5]
                 text_padded[i, :text.size(0)] = text
 
-            num_mels = batch[0][2].size(0)
-            max_target_len = max([x[2].size(1) for x in batch])
-            mel_padded = torch.FloatTensor(len(batch), num_mels, max_target_len)
-            mel_padded.zero_()
-            output_lengths = torch.LongTensor(len(batch))
+            if self.use_audio_seg:
+                seq_aud_len = batch[0][1].shape[0]
+                audios_seg = torch.FloatTensor(len(batch), 1, seq_aud_len)
+            else:
+                audios_seg = None
+
+            if self.use_mel:
+                num_mels = batch[0][2].size(0)
+                max_target_len = max([x[2].size(1) for x in batch])
+                mel_padded = torch.FloatTensor(len(batch), num_mels, max_target_len)
+                mel_padded.zero_()
+                output_lengths = torch.LongTensor(len(batch))
+            else:
+                mel_padded, output_lengths = None, None
 
             if self.use_coarse_mel:
                 max_coarse_target_len = max([x[4].size(1) for x in batch])
@@ -249,7 +297,7 @@ class _DataCollate():
                 coarse_rs = torch.FloatTensor(len(batch))
 
             else:
-                coarse_mel_padded = None
+                coarse_mel_padded, output_coarse_lengths, coarse_rs = None, None, None
 
             if self.use_attn_guide:
                 attn_guide_padded = torch.FloatTensor(len(batch), max_input_len, max_coarse_target_len)
@@ -257,7 +305,7 @@ class _DataCollate():
                 attn_mask_padded = torch.FloatTensor(len(batch), max_input_len, max_coarse_target_len)
                 attn_mask_padded.zero_()
             else:
-                attn_mask_padded, attn_mask_guide = None
+                attn_mask_padded, attn_guide_padded = None, None
 
             if self.use_attn_mask2:
                 attn_mask_padded2 = torch.FloatTensor(len(batch), max_input_len, max_coarse_target_len)
@@ -271,16 +319,23 @@ class _DataCollate():
             else:
                 tvmt_padded = None
 
+            mel_seg_tag = torch.LongTensor(len(batch), 2) if self.use_mel_seg_tag else None
+
             for i in range(len(ids_sorted_decreasing)):
-                mel = batch[ids_sorted_decreasing[i]][2]
-                mel_padded[i, :, :mel.size(1)] = mel
-                output_lengths[i] = mel.size(1)
+                if self.use_mel:
+                    mel = batch[ids_sorted_decreasing[i]][2]
+                    mel_padded[i, :, :mel.size(1)] = mel
+                    output_lengths[i] = mel.size(1)
 
                 if self.use_coarse_mel:
                     coarse_mel = batch[ids_sorted_decreasing[i]][4]
                     coarse_mel_padded[i, :, :coarse_mel.size(1)] = coarse_mel
                     output_coarse_lengths[i] = coarse_mel.size(1)
                     coarse_rs[i] = output_coarse_lengths[i].float() / input_lengths[i].float()
+
+                if self.use_audio_seg:
+                    audio = batch[i][1]
+                    audios_seg[i, :, :] = audio
 
                 if self.use_attn_guide:
                     attn_guide = batch[ids_sorted_decreasing[i]][6][:max_input_len, :max_coarse_target_len]
@@ -296,6 +351,9 @@ class _DataCollate():
                     tvmt = batch[ids_sorted_decreasing[i]][8]
                     tvmt_padded[i, :, :tvmt.size(1)] = tvmt
 
+                if self.use_mel_seg_tag and not self.is_valid:
+                    seg_tag = batch[ids_sorted_decreasing[i]][10]
+                    mel_seg_tag[i, :] = torch.LongTensor(seg_tag)
 
             new_batch = {
                 "text": text_padded,
@@ -308,7 +366,9 @@ class _DataCollate():
                 "coarse_olens": output_coarse_lengths,
                 "tvmt": tvmt_padded,
                 "coarse_length_ratio": coarse_rs,
-                "attn_mask2": attn_mask_padded2
+                "attn_mask2": attn_mask_padded2,
+                "mel_seg_tag": mel_seg_tag,
+                "audio_seg": audios_seg
             }
 
         else:
@@ -360,68 +420,3 @@ def guide_attention(text_lengths, mel_lengths, guide_g=0.2, r=None, c=None):
                     w = 1.0 - np.exp(-((float(n - N - 1) / N) ** 2 / (2.0 * (guide_g ** 2))))
                     W[n][t] = w
     return guide, mask
-
-
-# class TextMelCollate():
-#     """ Zero-pads model inputs and targets based on number of frames per setep
-#     """
-#     def __init__(self, conf, is_inference=False):
-#         self.conf = conf
-#         self.length_ratio_max = self.conf['data']['length_ratio_max']
-#         self.length_ratio_min = self.conf['data']['length_ratio_min']
-#
-#         self.is_inference = is_inference
-#         self.tvmt = TimeVaryingMetaTemplate(40)
-#
-#     def __call__(self, batch):
-#         """Collate's training batch from normalized text and mel-spectrogram
-#         PARAMS
-#         ------
-#         batch: [[text_normalized, mel_normalized], ...]
-#         """
-
-#
-#             if self.conf['train']['guided_attention']:
-#                 attn_guide_padded = torch.FloatTensor(len(batch), max_input_len, max_coarse_target_len)
-#                 attn_guide_padded.zero_()
-#                 attn_mask_padded = torch.FloatTensor(len(batch), max_input_len, max_coarse_target_len)
-#                 attn_mask_padded.zero_()
-#             for i in range(len(ids_sorted_decreasing)):
-#                 mel = batch[ids_sorted_decreasing[i]][1]
-#                 mel_padded[i, :, :mel.size(1)] = mel
-#                 coarse_mel = batch[ids_sorted_decreasing[i]][2]
-#                 coarse_mel_padded[i, :, :coarse_mel.size(1)] = coarse_mel
-#                 _ilen = len(batch[ids_sorted_decreasing[i]][0])
-#                 _olen = coarse_mel.size(1)
-#                 tvmt = self.tvmt(_olen, _ilen)
-#                 tvmt_padded[i, :, :coarse_mel.size(1)] = tvmt
-#                 gate_padded[i, mel.size(1)-1:] = 1
-#                 output_lengths[i] = mel.size(1)
-#                 coarse_output_lengths[i] = coarse_mel.size(1)
-#                 if self.conf['train']['guided_attention']:
-#                     attn_guide = batch[ids_sorted_decreasing[i]][3][:max_input_len, :max_coarse_target_len]
-#                     attn_mask = batch[ids_sorted_decreasing[i]][4][:max_input_len, :max_coarse_target_len]
-#                     attn_guide_padded[i, :max_input_len, :attn_guide.size(1)] = attn_guide
-#                     attn_mask_padded[i, :max_input_len, :attn_mask.size(1)] = attn_mask
-#                 ids.append(batch[ids_sorted_decreasing[i]][5])
-#
-#             xs = text_padded
-#             ilens = input_lengths
-#             ys = mel_padded
-#             coarse_ys = coarse_mel_padded
-#             coarse_olens = coarse_output_lengths
-#             olens = output_lengths
-#             gates = gate_padded
-#             tvmts = tvmt_padded
-#             if self.conf['train']['guided_attention']:
-#                 attn_masks = attn_mask_padded
-#                 attn_guides = attn_guide_padded
-#             else:
-#                 attn_masks = None
-#                 attn_guides = None
-#
-#             if self.length_ratio_max is not None:
-#                 length_ratio = olens.float() / ilens.float()
-#                 length_ratio = (length_ratio - self.length_ratio_min) / (self.length_ratio_max - self.length_ratio_min)
-#             else:
-#                 length_ratio = None
