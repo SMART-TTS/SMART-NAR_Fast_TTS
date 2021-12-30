c62e8e0fab31a2753f3c4e883abb26ffed90ea9d
leemoa12 <30678492+leemoa12@users.noreply.github.com>
2021-12-30 17:09:31 +0900

    minor

--- text/__init__.py
+++ text/__init__.py
@@ -3,6 +3,7 @@ import re
 from text import cleaners
 from text.symbols import symbols
 
+
 # Mappings from symbol to numeric ID and vice versa:
 _symbol_to_id = {s: i for i, s in enumerate(symbols)}
 _id_to_symbol = {i: s for i, s in enumerate(symbols)}
